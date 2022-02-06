6193ced8bb9c39794ec79708ae5e8b18af1396d0
Vincasmen <vincasmen13@gmail.com>
2022-02-06 21:06:59 +0100

    Změna toggle menu pro mobil zař.

--- JavaScript/Portfolio.js
+++ JavaScript/Portfolio.js
@@ -1,5 +1,10 @@
-function myFunction(x) {
-  x.classList.toggle("change");
+function myFunction() {
+  var x = document.getElementById("mobile-menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
 
 //Get the button:
